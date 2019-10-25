373b9659f819ed235868a2e66f2f66d9fe4e6513
董海朋 <haipeng.dong@doosan.com>
2019-10-25 14:59:20 +0800

    2019102502

--- src/main/java/com/multiseafoods/msapp/controller/LoginController.java
+++ src/main/java/com/multiseafoods/msapp/controller/LoginController.java
@@ -7,11 +7,15 @@ import com.multiseafoods.msapp.entity.User;
 import com.multiseafoods.msapp.service.UserService;
 import com.multiseafoods.msapp.utils.ResultUtil;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("user/")
@@ -24,11 +28,13 @@ public class LoginController {
 
 
     @PostMapping("login/")
+    @ApiOperation("登陆")
     public void login(User user) throws Exception{
         userService.login(user);
     }
 
     @PostMapping("logout/{username}")
+    @ApiOperation("注销")
     public Result logout(@PathVariable String username){
         tokenManager.deleteToken(username);
         return ResultUtil.ok();
@@ -36,7 +42,20 @@ public class LoginController {
 
     @PostMapping("checktoken")
     @Authorization
+    @ApiOperation("验证Token")
     public Result checkToken(){
         return ResultUtil.ok();
+    }
+
+    @PostMapping("register")
+    @ApiOperation("注册用户")
+    public Result save(@Valid User user, BindingResult bindResult){
+        if(bindResult.hasErrors()){
+            return ResultUtil.error(0,bindResult.getFieldError().getDefaultMessage());
+        }
+        if(userService.save(user) == 1){
+            return ResultUtil.ok();
+        }
+        return ResultUtil.error(-1,"未知错误，请联系管理员");
     }
 }
